f0420b8064904cafebb5b5d08e1090c47cd7f99c
gtm-serpro <guilherme.mota@serpro.gov.br>
2025-04-23 17:18:36 -0300

    shadcn

---
+++ app/routes/home.tsx
@@ -0,0 +1,15 @@
+import type { Route } from "./+types/home";
+import { Button } from "~/components/ui/button";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return (
+    <Button>Botão</Button>
+  );
+}
