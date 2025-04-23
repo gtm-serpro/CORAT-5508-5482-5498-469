4f1fa3e643658aa8bd48f0aad2bae8ba0c42a513
gtm-serpro <guilherme.mota@serpro.gov.br>
2025-04-23 17:59:54 -0300

    fix shadcn dist

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: App,
+})
+
+function App() {
+  return (
+    <div className="text-center">
+     <Button>teste</Button>
+    </div>
+  )
+}
